2c4a77e937e55a2187bb3188d0f0f9dca09f3e75
Raseek Sht <39650487+raseeksht@users.noreply.github.com>
2023-06-27 20:22:41 +0545

    used class and threading to search simultaneously

--- searchpdf/main.py
+++ searchpdf/main.py
@@ -1,38 +1,68 @@
-# importing all the required modules
 import PyPDF2
 import os
+import threading
 
 
-allfiles = os.listdir()
+class pdfSearch:
+    def __init__(self,searchDir):
+        self.searchDir = searchDir
+        self.found = []
+        self.threads = []
+        self.allfiles = []
 
-allfiles = list(filter(lambda name: name.endswith(".pdf"),allfiles))
+    def _getNestedFiles(self,dir):
+        files = os.listdir(dir)
+        pdfs = []
+        for file in files:
+            if os.path.isfile(f"{dir}/{file}"):
+                try:
+                    pdfs.append(f"{dir}/{file}")
+                except Exception:
+                    pass
+            else: #must be a folder
+                pdfs = self._getNestedFiles(f"{dir}/{file}")
+        try:
+            self.allfiles.extend(pdfs)
+        except:
+            pass
+    
+    def _searchInFile(self,file,searchTerm):
+        # creating a pdf reader object
+        reader = PyPDF2.PdfReader(file)
+
+        # print the number of pages in pdf file
+        print(f"searching in {file} pages=> " ,len(reader.pages))
+        for index,page in enumerate(reader.pages):
+            pagetext = page.extract_text()
+            if searchTerm in pagetext.lower():
+                # print(pagetext)
+
+                self.found.append((file,index))
+                break
+    
+    def search(self,term):
+        print(f"searching for {term}")
+        self._getNestedFiles(self.searchDir)
+        self.allfiles = list(filter(lambda name: name.endswith(".pdf"),self.allfiles))
+        for file in self.allfiles:
+            self.threads.append(threading.Thread(target=self._searchInFile,args=(file,term)))
+        
+        for i,_ in enumerate(self.allfiles):
+            self.threads[i].start()
+        for i,_ in enumerate(self.allfiles):
+            self.threads[i].join()
+
+        if not self.found:
+            print("\u001b[32mNot found\u001b[0m")
+        else:
+            print("\nCheck these pdf:")
+            for f in self.found:
+                print(f"\u001b[32m[+]\u001b[0m '{f[0]}' page no {f[1]}")
+
+
+searcher = pdfSearch(".")
+
 
 searchfor = input("search for : ")
 
-found = []
-
-for file in allfiles:
-    # creating a pdf reader object
-    reader = PyPDF2.PdfReader(file)
-
-    # print the number of pages in pdf file
-    print(f"searching in {file} pages=> " ,len(reader.pages))
-    for index,page in enumerate(reader.pages):
-        pagetext = page.extract_text()
-        if searchfor in pagetext.lower():
-            # print(pagetext)
-
-            found.append((file,index))
-            break
-
-
-if len(found) == 0:
-    print("not found")
-else:
-    print("\nCheck these pdf:")
-    for f in found:
-        print(f[0],"page no",f[1])
-
-
-    # print the text of the first page
-    # print(reader.pages[0].extract_text())
+searcher.search(searchfor)
